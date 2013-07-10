fdd075c1191101987e9dd0b7c5f53e97ab927c7f
spiraling <bozemanmonique@gmail.com>
2013-07-10 18:55:40 +0000

    reading from index

--- web.js
+++ web.js
@@ -4,8 +4,8 @@ var app = express.createServer(express.logger()),
     fs = require('fs');
 
 app.get('/', function(request, response) {
-	var content = fs.readFileSync(index.html);
-   response.send(content);
+	var buffer = fs.readFileSync(index.html);
+    response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
